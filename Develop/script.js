62400076c073079e57faf7ad93e9f36d1dad1b52
Brian OLeary <oleary.brian1@gmail.com>
2020-07-07 19:37:38 -0400

    comments

--- Develop/script.js
+++ Develop/script.js
@@ -21,46 +21,46 @@ const symbols = '!@#$%^&*()<>?:",./;-[]{}|`~=';
 let symbolArray = Array.from(symbols)
 
 
-generateBtn.addEventListener('click', () => {
+generateBtn.addEventListener('click', () => { //event listner starts the whole password generating
 
-  const passwordLength = prompt("Enter a value betweeen 8 and 128.")
-  if (passwordLength >= 8 && passwordLength <= 128) {
+  const passwordLength = prompt("Enter a value betweeen 8 and 128.") //defines password length
+  if (passwordLength >= 8 && passwordLength <= 128) { 
     
 
     const largeArray = []
-    const lowercase = confirm("Do you want your password to contain lowercase letters?")
+    const lowercase = confirm("Do you want your password to contain lowercase letters?") //use lowercase letters
     if (lowercase ) {
       largeArray.push(lowerCaseArray.join(''))
     }
 
-    const uppercase = confirm("Do you want your password to contain uppercase letters?")
+    const uppercase = confirm("Do you want your password to contain uppercase letters?") // use uppercase letters
     if (uppercase) {
       largeArray.push(upperCaseArray.join(''))
     }
-    const number = confirm("Do you want your password to contain numbers?")
+    const number = confirm("Do you want your password to contain numbers?") //use numbers
     if (number) {
       largeArray.push(numberArray.join(''))
     }
-    const symbol = confirm("Do you want your password to contain symbols?")
+    const symbol = confirm("Do you want your password to contain symbols?") //use symbols
     if (symbol) {
       largeArray.push(symbolArray.join(''))
     }
-    if(lowercase && uppercase && number && symbol ){
+    if(lowercase && uppercase && number && symbol ){ //if no charecter groups are accepted, kick you out
     const megaArray = [largeArray.join('')]
     const superArray =megaArray[0].split("")
 
-    let finalPassword = "";
+    let finalPassword = ""; // randomize a final password
     for (i = 0; i < passwordLength; i++) {
       var randomValue = superArray[Math.floor(Math.random() * superArray.length)];
       finalPassword+=randomValue
     }
     passwordText.value = finalPassword;
   }
-  else{
+  else{ //alert
     alert("you must select atleast one option.")
   }
   }
-  else {
+  else { //alert
     alert("Password must be a numeric value and between 8 and 124 charecters")
   }
 }
